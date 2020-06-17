b1876989d9d5248f49e2beaff1d96bb0c4335cf5
Sarah Peters <sarahpieters15@gmail.com>
2020-06-17 10:55:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,8 +5,8 @@ def third_challenge
         :matriarch => {name: "Lady Montague", age: "54"},
         :hero => {name: "Romeo", age: "15", status: "alive"},
         :hero_friends => [
-           {:age=>"17", :attitude=>"worried", :name=>"Benvolio"},
-           {:age=>"18", :attitude=>"hot-headed", :name=>"Mercutio"}
+           {name: "Benvolio", age: "17", attitude: "worried"},
+           {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
      },
      :capulet => {
@@ -14,8 +14,8 @@ def third_challenge
         :matriarch => {name: "Lady Capulet", age: "51"},
         :heroine => {name: "Juliet", age: "15", status: "alive"},
         :heroine_friends => [
-            {:age=>"30", :attitude=>"confused", :name=>"Steven"},
-            {:age=>"44", :attitude=>"worried", :name=>"Nurse"}
+            {name: "Steven", age: "30", attitude: "confused"},
+            {name: "Nurse", age: "44", attitude: "worried"}
         ]
      }
   }
